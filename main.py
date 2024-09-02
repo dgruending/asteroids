b6047fe1fce9bb52a32e53b0848867470ced7301
Dominik Kersting <d.kersting@posteo.de>
2024-09-02 07:43:27 +0200

    Setup game time

--- main.py
+++ main.py
@@ -2,9 +2,24 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    delta_time = 0
+
+    # Game loop
+    while True:
+        # Exit strategy
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # Black screen
+        screen.fill("black")
+        pygame.display.flip()
+
+        delta_time = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
